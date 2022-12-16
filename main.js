35b618a28d0e0e1304d033ad6092e3975132ebf2
WaiLinnAungIsHandsome <waia00198@gmail.com>
2022-12-16 12:54:38 +0630

    home page

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.scss'
+import ScrollReveal from 'scrollreveal'
+
+
+let toDown = {
+    distance: "50px",
+    origin: 'top',
+    delay: 1000,
+    duration: 1500
+}
+ScrollReveal().reveal(".top_down",toDown)
