f4916b26ecda11e751720b316e10f29aaaa10e04
WaiLinnAung18 <waia00198@gmail.com>
2022-12-18 19:19:32 +0630

    add services page

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
-import './style.scss'
-import 'waypoints/lib/noframework.waypoints'
-import ScrollReveal from 'scrollreveal'
-import Typed from 'typed.js'
+import "./style.scss";
+import "waypoints/lib/noframework.waypoints";
+import ScrollReveal from "scrollreveal";
+import Typed from "typed.js";
 
 let options = {
-    strings:['Foods...','Drinks...','Time...'],
-    typeSpeed:90,
-    backSpeed:90,
-    delay:900,
-    loop:true
-}
+  strings: ["Foods...", "Drinks...", "Time..."],
+  typeSpeed: 90,
+  backSpeed: 90,
+  delay: 900,
+  loop: true,
+};
 
-new Typed ("#type",options)
+new Typed("#type", options);
 
 let toDown = {
-    distance: "50px",
-    origin: 'top',
-    interval: 300,
-    duration: 1500
-}
-ScrollReveal().reveal(".top_down",toDown)
+  distance: "50px",
+  origin: "top",
+  interval: 300,
+  duration: 1500,
+};
+ScrollReveal().reveal(".top_down", toDown);
 
 let toLeft = {
-    distance: "50px",
-    origin: 'right',
-    interval: 300,
-    duration: 1500
-}
-ScrollReveal().reveal(".to-left",toLeft)
-
+  distance: "50px",
+  origin: "right",
+  interval: 300,
+  duration: 1500,
+};
+ScrollReveal().reveal(".to-left", toLeft);
 
 let toRight = {
-    distance: "50px",
-    origin: 'left',
-    interval: 300,
-    duration: 1500
-}
-ScrollReveal().reveal(".to-right",toRight)
+  distance: "50px",
+  origin: "left",
+  interval: 300,
+  duration: 1500,
+};
+ScrollReveal().reveal(".to-right", toRight);
 
 new Waypoint({
-    element: document.getElementById('about'),
-    handler: function(direction) {
-        let oldActiveLink = document.querySelector(".nav-link.active");
-        oldActiveLink.classList.remove("active")
-        let newActiveLink = document.querySelector(`[href="#about"]`);
-        newActiveLink.classList.add("active");
+  element: document.getElementById("home-content"),
+  handler: function (direction) {
+    let oldNavLink = document.querySelector(".nav-link.active");
+    if (oldNavLink != null) {
+      oldNavLink.classList.remove("active");
+    }
+    let currentNavLink = document.querySelector(`[href="#home"]`);
+    currentNavLink.classList.add("active");
+  },
+  offset: "10%",
+});
+
+let pages = ["about", "services", "menu", "contact"];
+
+pages.forEach((page) => {
+  new Waypoint({
+    element: document.getElementById(page),
+    handler: function (direction) {
+      let oldActiveLink = document.querySelector(".nav-link.active");
+      oldActiveLink.classList.remove("active");
+      let newActiveLink = document.querySelector(`[href="#${page}"]`);
+      newActiveLink.classList.add("active");
     },
-    offset: 20 
-  })
+    offset: "50%",
+  });
+});
